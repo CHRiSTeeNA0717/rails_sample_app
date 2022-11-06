f712860ad39de67241cb6a283f7fbd9a62983e8b
CHRiSTeeNA0717 <103508472+CHRiSTeeNA0717@users.noreply.github.com>
2022-11-07 06:02:39 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,6 +6,11 @@ app_path = "/var/www/rails_sample_app"
 pid_path = "#{app_path}/tmp/pids/unicorn.pid"
 listen_path = "#{app_path}/tmp/sockets/unicorn.sock"
 
+working_directory app_path
+pid pid_path
+listen listen_path, backlog: 64
+preload_app true
+
 before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
